29866ee62cb55dd0aa3d0e0f3ec76e4db7555c4d
sumpter32 <sumpter32@gmail.com>
2023-01-09 09:49:35 +0800

    new server

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://georgec.onrender.com/', {
+    const response = await fetch('https://response-email.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
